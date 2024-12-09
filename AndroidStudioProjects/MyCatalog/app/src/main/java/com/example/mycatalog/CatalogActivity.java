14a23e81f5e42720f5e4ce97ccd39129abfe1df7
JacoboBecerra <jbecerral23@fpcoruna.afundacion.org>
2024-12-09 12:27:13 +0100

    Segundo commit

--- AndroidStudioProjects/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ AndroidStudioProjects/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,6 +1,9 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,21 @@ public class CatalogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.catalog_activity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Cargamos el xml
+        setContentView(R.layout.activity_detail);
+
+
+        // Conecta el botón usando su ID
+        Button navigateButton = findViewById(R.id.btnNavigateToDetail);
+
+        // Configura el evento onClick
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lanza DetailActivity
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
